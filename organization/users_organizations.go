d3c9895779371c64aaebe8a5539c4f6925003574
iamagf <agf0710@gmail.com>
2022-11-07 20:42:11 -0600

    Added update instead of new post

--- organization/users_organizations.go
+++ organization/users_organizations.go
@@ -7,38 +7,6 @@ import (
 	"encore.dev/storage/sqldb"
 )
 
-type PostUserToOrganizationParams struct {
-	OrganizationPubKey string `json:"organization_pub_key"`
-	UserPubKey         string `json:"user_pub_key"`
-	UserInformation    string `json:"content"`
-}
-
-type UserPosted struct {
-	Successful bool `json:"success"`
-}
-
-//encore:api public method=POST
-func PostUserToOrganization(ctx context.Context, params *PostUserToOrganizationParams) (*UserPosted, error) {
-
-	registerID := commons.GenerateID()
-	organizationPubKey := params.OrganizationPubKey
-	userPubKey := params.UserPubKey
-	userInformation := params.UserInformation
-
-	// TODO:
-	// Use some proper ORM instead of raw SQL querie
-	_, err := sqldb.Exec(ctx, "INSERT INTO user_organization (id, organization_pub_key, user_pub_key, content) VALUES ($1, $2, $3, $4);",
-		registerID,
-		organizationPubKey,
-		userPubKey,
-		userInformation)
-	if err != nil {
-		return nil, err
-	}
-
-	return &UserPosted{Successful: true}, nil
-}
-
 type GetUsersFromOrganizationParams struct {
 	OrganizationId string `json:"organizationId"`
 }
@@ -77,4 +45,62 @@ func GetUsersFromOrganization(ctx context.Context, params *GetUsersFromOrganizat
 
 	response.Users = users
 	return &response, nil
+}
+
+type PostUserToOrganizationParams struct {
+	OrganizationPubKey string `json:"organization_pub_key"`
+	UserPubKey         string `json:"user_pub_key"`
+	UserInformation    string `json:"content"`
+}
+
+type UserPosted struct {
+	Successful string `json:"success"`
+}
+
+//encore:api public method=POST
+func PostUserToOrganization(ctx context.Context, params *PostUserToOrganizationParams) (*UserPosted, error) {
+
+	registerID := commons.GenerateID()
+	organizationPubKey := params.OrganizationPubKey
+	userPubKey := params.UserPubKey
+	userInformation := params.UserInformation
+
+	paramsGetUsersFromOrganizationParams := GetUsersFromOrganizationParams{OrganizationId: organizationPubKey}
+
+	previousData, err := GetUsersFromOrganization(ctx, &paramsGetUsersFromOrganizationParams)
+	if err != nil {
+		return nil, err
+	}
+
+	usersRegistered := previousData.Users
+	userPreviouslyRegistered := false
+
+	for _, user := range usersRegistered {
+		if user.UserId == userPubKey {
+			userPreviouslyRegistered = true
+		}
+	}
+
+	if userPreviouslyRegistered {
+		_, err := sqldb.Exec(ctx, "UPDATE user_organization SET content = $1 WHERE (organization_pub_key=$2 and user_pub_key=$3);",
+			userInformation,
+			organizationPubKey,
+			userPubKey)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return &UserPosted{Successful: "update"}, nil
+	} else {
+		_, err := sqldb.Exec(ctx, "INSERT INTO user_organization (id, organization_pub_key, user_pub_key, content) VALUES ($1, $2, $3, $4);",
+			registerID,
+			organizationPubKey,
+			userPubKey,
+			userInformation)
+		if err != nil {
+			return nil, err
+		}
+		return &UserPosted{Successful: "post"}, nil
+	}
 }
